6d64df0964b3a56f12ba445a1b845b80772fdccb
SalminWEB9-1709 <janicesangma@gmail.com>
2024-04-20 15:05:25 +0600

    add 55-1 mongodb atlas account for simple crud server

--- index.js
+++ index.js
@@ -3,4 +3,38 @@ const cors = require ('cors')
 const app = express ();
 const port = process.env.PORT  || 5000;
 
+
+//this is my middle waressss!
+
 app.use(cors());
+app.use(express.json())
+
+
+const{ mongoclient, ServerApiVersion } = require ('mongodb')
+const uri = 'mongdb+srv://elfkimberly042:bYTMZiDbzHl55Csl@cluster0.vlpdl0j.mongdb.net/?retryWrites=true&w=majority';
+
+
+async function run (){
+    try{
+        await client.connect();
+       await client. db ('admin').command({ping:1});
+       console.log('pinged your deployment.your successfully connected to mangodb!');
+
+    }
+    finally{
+        //await client .close ();
+    }
+}
+run().catch(console.log);
+
+app.get ('/', (req, res) => {
+    res.send ('this my simple and easy server running side')
+})
+
+// get data from the database
+
+app.listen(port , () => {
+    console.log(`simple crud is running on port,${port}`)
+})
+
+
