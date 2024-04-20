aa8bd420538a55077026ec53ba46c652658254ef
SalminWEB9-1709 <janicesangma@gmail.com>
2024-04-20 18:25:19 +0600

    55.5 Create Read And Delete And Show Remaining Users

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const cors = require ('cors')
-const{ mongoclient, ServerApiVersion } = require ('mongodb');
+const{ MongoClient, ServerApiVersion, ObjectId } = require ('mongodb');
 const app = express ();
 const port = process.env.PORT  || 5000;
 
@@ -8,7 +8,7 @@ const port = process.env.PORT  || 5000;
 //this is my middle waressss!
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 
 
@@ -25,7 +25,40 @@ const client = new MongoClient (uri, {
 async function run (){
     try{
         await client.connect();
-       await client. db ('admin').command({ping:1});
+       
+        //const database = client.db("usersDB");
+       // const userCollection = database.collection("users");
+      const userCollection = client.db('userDB').collection('users');
+
+      app.get('/users',  async (req,res)=>{
+          const cursor = userCollection.find()
+         const result = await cursor.toArray();
+         res.send(result);
+      })
+
+
+
+
+      app.post ('/users', async ( req, res) => {
+        const user = req.body;
+        console.log('notun', user);
+
+         const result = await userCollection.insertOne(user);
+         res.send(result);
+      });
+
+      app.delete('/users/:id', (req,res) => {
+        const id = req.params.id;
+        console.log('please delete from database', id);
+        const query = {_id: new ObjectId (id)};
+
+        const result = await userCollection.deleteOne(query);
+        res.send(result);
+      })
+
+      
+      
+      await client. db ('admin').command({ping:1});
        console.log('pinged your deployment.your successfully connected to mangodb!');
 
     }
@@ -36,7 +69,8 @@ async function run (){
 run().catch(console.log);
 
 app.get ('/', (req, res) => {
-    res.send ('this my simple and easy server running side')
+    res.send ('this my simple and easy server running side');
+
 })
 
 // get data from the database
